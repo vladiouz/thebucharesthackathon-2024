1db220015af4ae22ca18d54005a2085eb4a3d500
florin.bc <florin.barbalan@atta.systems>
2024-04-13 21:40:09 +0300

    Add dotenv for secrets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
-import { BackendService } from "@genezio-sdk/the-bucharest-hackathon-2024";
 import "./App.css";
 import Form from "./components/form/Form";
 
 function App() {
-  const [name, setName] = useState("");
-  const [response, setResponse] = useState("");
-
-  async function sayHello() {
-    setResponse(await BackendService.hello(name));
-  }
-
   return (
     <>
       <div className="app">
